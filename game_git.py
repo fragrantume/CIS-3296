f407dbf62dcad3dcd362f05a98f350186badc1bf
fragrantume <fragrantume@protonmail.com>
2026-09-03 11:44:04 +0900

    Improved version: less junk, less parameters

--- game_git.py
+++ game_git.py
@@ -1,36 +1,39 @@
-def score(i, r, d, temp = 10, final = 10, initial_d = 0):
-    print(round(temp))
- 
+def score(d, i=10, total=0):
+    # Add current score to total
+    total += i
+
+    # Print currect score 
+    print(i)
+
+    # Stop recursion when D reaches 1
     if d == 1:
-        print(f"Your final score: {final}")
- 
-        if initial_d >= 4:
-            if final >= 80:
-                print("You are qualified.")
-            else:
-                print("You are NOT qualified.")
- 
-        return i
- 
-    return r * score(i, r, d - 1, temp * r, final + round(temp * r), initial_d)
- 
+        print(f"Your final score: {total}")
+
+        return total
+
+    return score(d - 1, round(i * 1.2), total)
+
 def main():
- 
-    i = 10
-    r = 1.2
- 
     pressed = False
- 
+
     while not pressed:
+        # Ask the user for their D
         print("To exit, enter number less than 2, or press Enter")
- 
         d = int(input("Enter your D: "))
- 
+
         if d >= 2:
-            score(i, r, d, initial_d=d)
+            final = score(d)
+
+            # Only check qualification if D is at least 4   
+            if d >= 4:
+                if final >= 80:
+                    print("You are qualified.")
+                else:
+                    print("You are NOT qualified.")
+
         else:
+            # Exit if D is less than 2
             pressed = True
- 
- 
+            
 if __name__ == '__main__':
     main()
