7349f721a42087f0a43278a61c553e195232897a
Andrei <fragrantume@protonmail.com>
2026-09-12 21:09:23 +0900

    Remove unnecessary paddings

--- scoring.py
+++ scoring.py
@@ -1,7 +1,6 @@
 QUALIFYING_ROUND = 4
 QUALIFYING_SCORE = 80
 
-
 def score(d, i, rate, total=0):
     # Cumulative score after d rounds, starting with initial score i and growth by rate.
     total += i
@@ -9,11 +8,9 @@ def score(d, i, rate, total=0):
         return total
     return score(d - 1, i * rate, rate, total)
 
-
 def cumulative(team, d):
     # Cumulative score for a team after d rounds. 
     return score(d, team["initial_score"], team["growth_rate"])
-
 
 def is_qualified(team):
     # Qualifies if the cumulative score after QUALIFYING_ROUND is at least QUALIFYING_SCORE.
